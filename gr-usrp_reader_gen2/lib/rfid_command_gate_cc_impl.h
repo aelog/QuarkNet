d19eec1cc050bf5b684a6661e5ee0f6a79a67585
Elvis Angelaccio <elvis.git@aelog.org>
2016-05-16 17:55:27 +0200

    Forgot setter

--- gr-usrp_reader_gen2/lib/rfid_command_gate_cc_impl.h
+++ gr-usrp_reader_gen2/lib/rfid_command_gate_cc_impl.h
@@ -71,6 +71,8 @@ namespace gr {
            gr_vector_int &ninput_items,
            gr_vector_const_void_star &input_items,
            gr_vector_void_star &output_items);
+
+      void set_ctrl_out(const gr::msg_queue::sptr msgq) { d_ctrl_out = msgq; }
     };
 
   } // namespace usrp_reader_gen2
