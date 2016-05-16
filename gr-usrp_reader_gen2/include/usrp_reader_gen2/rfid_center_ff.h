6beed873d39dc5bbc69a3a62a200c5f53b433aa9
Elvis Angelaccio <elvis.git@aelog.org>
2016-05-16 15:36:03 +0200

    Introduce rfid_center_ff sync block
    Created with:
    
    'gr_modtool add -t sync rfid_center_ff'
    
    and using 'int samples_per_pulse' as the arguments list.

---
+++ gr-usrp_reader_gen2/include/usrp_reader_gen2/rfid_center_ff.h
@@ -0,0 +1,56 @@
+/* -*- c++ -*- */
+/* 
+ * Copyright 2016 <+YOU OR YOUR COMPANY+>.
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+
+#ifndef INCLUDED_USRP_READER_GEN2_RFID_CENTER_FF_H
+#define INCLUDED_USRP_READER_GEN2_RFID_CENTER_FF_H
+
+#include <usrp_reader_gen2/api.h>
+#include <gnuradio/sync_block.h>
+
+namespace gr {
+  namespace usrp_reader_gen2 {
+
+    /*!
+     * \brief <+description of block+>
+     * \ingroup usrp_reader_gen2
+     *
+     */
+    class USRP_READER_GEN2_API rfid_center_ff : virtual public gr::sync_block
+    {
+     public:
+      typedef boost::shared_ptr<rfid_center_ff> sptr;
+
+      /*!
+       * \brief Return a shared_ptr to a new instance of usrp_reader_gen2::rfid_center_ff.
+       *
+       * To avoid accidental use of raw pointers, usrp_reader_gen2::rfid_center_ff's
+       * constructor is in a private implementation
+       * class. usrp_reader_gen2::rfid_center_ff::make is the public interface for
+       * creating new instances.
+       */
+      static sptr make(int samples_per_pulse);
+    };
+
+  } // namespace usrp_reader_gen2
+} // namespace gr
+
+#endif /* INCLUDED_USRP_READER_GEN2_RFID_CENTER_FF_H */
+
--- gr-usrp_reader_gen2/swig/usrp_reader_gen2_swig.i
+++ gr-usrp_reader_gen2/swig/usrp_reader_gen2_swig.i
@@ -8,6 +8,9 @@
 %include "usrp_reader_gen2_swig_doc.i"
 
 %{
+#include "usrp_reader_gen2/rfid_center_ff.h"
 %}
 
 
+%include "usrp_reader_gen2/rfid_center_ff.h"
+GR_SWIG_BLOCK_MAGIC2(usrp_reader_gen2, rfid_center_ff);
