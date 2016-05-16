f4ae44338eccd73bc0ee1f298c02f98049498054
Elvis Angelaccio <elvis.git@aelog.org>
2016-05-16 17:59:30 +0200

    Style++

--- gr-usrp_reader_gen2/lib/rfid_command_gate_cc_impl.h
+++ gr-usrp_reader_gen2/lib/rfid_command_gate_cc_impl.h
@@ -68,9 +68,9 @@ namespace gr {
       void forecast (int noutput_items, gr_vector_int &ninput_items_required);
 
       int general_work(int noutput_items,
-           gr_vector_int &ninput_items,
-           gr_vector_const_void_star &input_items,
-           gr_vector_void_star &output_items);
+                       gr_vector_int &ninput_items,
+                       gr_vector_const_void_star &input_items,
+                       gr_vector_void_star &output_items);
 
       void set_ctrl_out(const gr::msg_queue::sptr msgq) { d_ctrl_out = msgq; }
     };
