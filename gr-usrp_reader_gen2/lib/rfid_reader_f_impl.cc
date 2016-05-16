b1458fe71bd955e6ecc9b74ca9c5bb56cf9a4f85
Elvis Angelaccio <elvis.git@aelog.org>
2016-05-16 19:03:20 +0200

    Introduce rfid_reader_f general block
    argument lists: int sample_rate

---
+++ gr-usrp_reader_gen2/lib/rfid_reader_f_impl.cc
@@ -0,0 +1,80 @@
+/* -*- c++ -*- */
+/* 
+ * Copyright 2016 <+YOU OR YOUR COMPANY+>.
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <gnuradio/io_signature.h>
+#include "rfid_reader_f_impl.h"
+
+namespace gr {
+  namespace usrp_reader_gen2 {
+
+    rfid_reader_f::sptr
+    rfid_reader_f::make(int sample_rate)
+    {
+      return gnuradio::get_initial_sptr
+        (new rfid_reader_f_impl(sample_rate));
+    }
+
+    /*
+     * The private constructor
+     */
+    rfid_reader_f_impl::rfid_reader_f_impl(int sample_rate)
+      : gr::block("rfid_reader_f",
+              gr::io_signature::make(<+MIN_IN+>, <+MAX_IN+>, sizeof(<+ITYPE+>)),
+              gr::io_signature::make(<+MIN_OUT+>, <+MAX_OUT+>, sizeof(<+OTYPE+>)))
+    {}
+
+    /*
+     * Our virtual destructor.
+     */
+    rfid_reader_f_impl::~rfid_reader_f_impl()
+    {
+    }
+
+    void
+    rfid_reader_f_impl::forecast (int noutput_items, gr_vector_int &ninput_items_required)
+    {
+      /* <+forecast+> e.g. ninput_items_required[0] = noutput_items */
+    }
+
+    int
+    rfid_reader_f_impl::general_work (int noutput_items,
+                       gr_vector_int &ninput_items,
+                       gr_vector_const_void_star &input_items,
+                       gr_vector_void_star &output_items)
+    {
+      const <+ITYPE+> *in = (const <+ITYPE+> *) input_items[0];
+      <+OTYPE+> *out = (<+OTYPE+> *) output_items[0];
+
+      // Do <+signal processing+>
+      // Tell runtime system how many input items we consumed on
+      // each input stream.
+      consume_each (noutput_items);
+
+      // Tell runtime system how many output items we produced.
+      return noutput_items;
+    }
+
+  } /* namespace usrp_reader_gen2 */
+} /* namespace gr */
+
--- gr-usrp_reader_gen2/swig/usrp_reader_gen2_swig.i
+++ gr-usrp_reader_gen2/swig/usrp_reader_gen2_swig.i
@@ -13,6 +13,7 @@
 #include "usrp_reader_gen2/rfid_command_gate_cc.h"
 #include "usrp_reader_gen2/rfid_reader_decoder.h"
 #include "usrp_reader_gen2/rfid_tag_decoder_f.h"
+#include "usrp_reader_gen2/rfid_reader_f.h"
 %}
 
 
@@ -26,3 +27,5 @@ GR_SWIG_BLOCK_MAGIC2(usrp_reader_gen2, rfid_command_gate_cc);
 GR_SWIG_BLOCK_MAGIC2(usrp_reader_gen2, rfid_reader_decoder);
 %include "usrp_reader_gen2/rfid_tag_decoder_f.h"
 GR_SWIG_BLOCK_MAGIC2(usrp_reader_gen2, rfid_tag_decoder_f);
+%include "usrp_reader_gen2/rfid_reader_f.h"
+GR_SWIG_BLOCK_MAGIC2(usrp_reader_gen2, rfid_reader_f);
